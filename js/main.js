3712997fba4ae3b00d7114fb178881b85c9b8585
Marcelo Gelato <gelatomarcelo77@gmail.com>
2026-09-25 21:37:35 -0300

    menu collapse

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@
         }, 1);
     };
     spinner();
-    
-    
+
+
     // Initiate the wowjs
     new WOW().init();
 
@@ -24,8 +24,8 @@
             $('.navbar').removeClass('position-fixed bg-dark shadow-sm');
         }
     });
-    
-    
+
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
@@ -35,9 +35,25 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
-    
+
 })(jQuery);
 
+// Menu collapse
+// Selecciona todos los links dentro del menú colapsado
+document.querySelectorAll('.navbar-collapse .nav-link, .navbar-collapse .dropdown-item')
+    .forEach(function (link) {
+        link.addEventListener('click', function () {
+            // Si es un dropdown-toggle, no colapsar
+            if (!link.classList.contains('dropdown-toggle')) {
+                var navbarCollapse = document.querySelector('.navbar-collapse.show');
+                if (navbarCollapse) {
+                    var bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+                        toggle: true
+                    });
+                }
+            }
+        });
+    });
